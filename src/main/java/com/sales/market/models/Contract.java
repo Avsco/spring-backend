b5752bf674dadaf7cf5640c785bf932aebdd36c2
Alan Benavides <bruno@coastt.org>
2021-08-30 15:33:38 -0400

    create: entity contract

---
+++ src/main/java/com/sales/market/models/Contract.java
@@ -0,0 +1,48 @@
+package com.sales.market.models;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.util.Date;
+
+@Entity
+public class Contract extends ModelBase {
+    @ManyToOne(optional = false)
+    private Employee employee;
+    @OneToOne(optional = false)
+    private Position position;
+    private Date initDate;
+    private Date endDate;
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Date getInitDate() {
+        return initDate;
+    }
+
+    public void setInitDate(Date initDate) {
+        this.initDate = initDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
